edb70ef876f3f963de2596e5e21ba67c1df32903
tarista13 <tyarista13@gmail.com>
2024-11-29 02:31:09 -0600

    Added project description page

--- src/app/components/all-projects/all-projects.component.ts
+++ src/app/components/all-projects/all-projects.component.ts
@@ -1,36 +1,40 @@
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { ProjectDescriptionComponent } from '../project-description/project-description.component';
 
 export interface AllProjects {
   year: string;
   project: string;
+  description: string;
   madeAt: string;
   builtWith: string;
   link: string;
+  image: string;
 }
 
 const PROJECT_DATA: AllProjects[] = [
-  { year: 'Summer 2024 - Present', project: 'Dealer Self Service/Quote Builder', madeAt: 'Steelcase', builtWith: 'ASP.NET Core', link:'https://www.steelcase.com/'},
-  { year: 'Fall 2024 - Present', project: 'Calvin EMR', madeAt: 'CS396/398', builtWith: 'Angular', link:'https://calvin-emr.web.app/about-us'},
-  { year: 'Fall 2024', project: 'Music Translator', madeAt: 'CS336', builtWith: 'Angular', link:'https://github.com/CS336-Final-Project'},
-  { year: 'Fall 2024', project: 'Stats Project', madeAt: 'STAT245', builtWith: 'R', link:''},
-  { year: 'Fall 2024', project: 'BlackJack', madeAt: 'DATA202', builtWith: 'Python', link:''},
-  { year: 'Fall 2024', project: 'Relationship between Average Fastball Speed & Tommy John', madeAt: 'DATA202', builtWith: 'Python', link:'https://github.com/tarista13/DATA202/blob/main/Midterm%20Project/data-202-midterm-project.ipynb'},
-  { year: 'Fall 2024', project: 'Assessing the Relationship Between Glycemic Status & Triglyceride Levels', madeAt: 'STAT245', builtWith: 'R', link:'https://github.com/tarista13/STAT245/blob/main/Exam1/Arista-Test1Revisions.pdf'},
-  { year: 'Fall 2024', project: 'Chat-app', madeAt: 'CS336', builtWith: 'Angular & Firebase', link:'https://github.com/24FA-CS336-org/assignment-15-tarista13'},
-  { year: 'Fall 2024', project: 'Pizza Ordering Form', madeAt: 'CS336', builtWith: 'HTML, CSS, JavaScript', link:'https://github.com/24FA-CS336-org/assignment-5-tarista13'},
-  { year: 'In Progress', project: 'Baseball Analytics(In Progress)', madeAt: 'Personal', builtWith: '...', link:''},
-  { year: 'In Progress', project: 'AI Essay Revision(In Progress)', madeAt: 'Personal', builtWith: '...', link:''},
-  { year: 'Summer 2024', project: 'D3 KPI', madeAt: 'Steelcase', builtWith: 'Tableau', link:'https://www.steelcase.com/'},
-  { year: 'Spring 2024', project: 'AI Generative Essay Revisions', madeAt: 'CS376', builtWith: 'Python', link:'https://github.com/tarista13/CS376-ML/tree/main/finalProject'},
-  { year: 'Fall 2023', project: 'Mindful Knights', madeAt: 'CS232', builtWith: 'React Native', link:'https://github.com/calvin-cs262-fall2023-teamh'},
-  { year: 'Summer 2023', project: 'Password Standard', madeAt: 'Steelcase', builtWith: 'Azure', link:'https://www.steelcase.com/'},
-  { year: 'Fall 2022', project: 'AI Mancala', madeAt: 'CS212', builtWith: 'C#', link:'https://github.com/tarista13/CS212/tree/main/Prog5'},
-  { year: 'Fall 2022', project: 'Fractal Fern', madeAt: 'CS212', builtWith: 'C#', link:'https://github.com/tarista13/CS212/tree/main/Fern'},
-  { year: 'Fall 2022', project: 'Dutch Bingo', madeAt: 'CS212', builtWith: 'C#', link:'https://github.com/tarista13/CS212/tree/main/Prog4'},
-  { year: 'Fall 2022', project: 'Babble', madeAt: 'CS212', builtWith: 'C#', link:'https://github.com/tarista13/CS212/tree/main/Project_2'},
-  { year: 'Fall 2021', project: 'Brick Breaker', madeAt: 'CS108', builtWith: 'Python', link:'https://github.com/tarista13/Brick-Breaker'}
+  { year: 'Summer 2024 - Present', project: 'Dealer Self Service/Quote Builder', description:'', madeAt: 'Steelcase', builtWith: 'ASP.NET Core', link:'https://www.steelcase.com/', image:''},
+  { year: 'Fall 2024 - Present', project: 'Calvin EMR', description:'', madeAt: 'CS396/398', builtWith: 'Angular', link:'https://calvin-emr.web.app/about-us', image:''},
+  { year: 'Fall 2024', project: 'Music Translator', description:'', madeAt: 'CS336', builtWith: 'Angular & Firebase', link:'https://github.com/CS336-Final-Project', image:''},
+  { year: 'In Progress', project: 'Sports Card Analyzer', description:'', madeAt: 'Personal', builtWith: 'ReactNative & Azure', link:'https://github.com/Sport-Card-Price-Analyzer', image:''},
+  { year: 'Fall 2024', project: 'Stats Project', description:'', madeAt: 'STAT245', builtWith: 'R', link:'', image:''},
+  { year: 'Fall 2024', project: 'BlackJack', description:'', madeAt: 'DATA202', builtWith: 'Python', link:'', image:''},
+  { year: 'Fall 2024', project: 'Relationship between Average Fastball Speed & Tommy John',description:'', madeAt: 'DATA202', builtWith: 'Python', link:'https://github.com/tarista13/DATA202/blob/main/Midterm%20Project/data-202-midterm-project.ipynb', image:''},
+  { year: 'Fall 2024', project: 'Assessing the Relationship Between Glycemic Status & Triglyceride Levels',description:'', madeAt: 'STAT245', builtWith: 'R', link:'https://github.com/tarista13/STAT245/blob/main/Exam1/Arista-Test1Revisions.pdf', image:''},
+  { year: 'Fall 2024', project: 'Chat-app', description:'', madeAt: 'CS336', builtWith: 'Angular & Firebase', link:'https://github.com/24FA-CS336-org/assignment-15-tarista13', image:''},
+  { year: 'Fall 2024', project: 'Pizza Ordering Form', description:'', madeAt: 'CS336', builtWith: 'HTML, CSS, JavaScript', link:'https://github.com/24FA-CS336-org/assignment-5-tarista13', image:''},
+  { year: 'In Progress', project: 'Baseball Analytics(In Progress)', description:'', madeAt: 'Personal', builtWith: 'Django', link:'https://github.com/Sports-Performance-Prediction', image:''},
+  { year: 'In Progress', project: 'AI Essay Revision(In Progress)', description:'', madeAt: 'Personal', builtWith: 'React', link:'https://github.com/AI-Essay-Revision', image:''},
+  { year: 'Summer 2024', project: 'D3 KPI', description:'', madeAt: 'Steelcase', builtWith: 'Tableau', link:'https://www.steelcase.com/', image:''},
+  { year: 'Spring 2024', project: 'AI Generative Essay Revisions', description:'', madeAt: 'CS376', builtWith: 'Python', link:'https://github.com/tarista13/CS376-ML/tree/main/finalProject', image:''},
+  { year: 'Fall 2023', project: 'Mindful Knights', description:'', madeAt: 'CS232', builtWith: 'React Native', link:'https://github.com/calvin-cs262-fall2023-teamh', image:''},
+  { year: 'Summer 2023', project: 'Password Standard', description:'', madeAt: 'Steelcase', builtWith: 'Azure', link:'https://www.steelcase.com/', image:''},
+  { year: 'Fall 2022', project: 'AI Mancala', description:'', madeAt: 'CS212', builtWith: 'C#', link:'https://github.com/tarista13/CS212/tree/main/Prog5', image:''},
+  { year: 'Fall 2022', project: 'Fractal Fern', description:'', madeAt: 'CS212', builtWith: 'C#', link:'https://github.com/tarista13/CS212/tree/main/Fern', image:''},
+  { year: 'Fall 2022', project: 'Dutch Bingo', description:'', madeAt: 'CS212', builtWith: 'C#', link:'https://github.com/tarista13/CS212/tree/main/Prog4', image:''},
+  { year: 'Fall 2022', project: 'Babble', description:'', madeAt: 'CS212', builtWith: 'C#', link:'https://github.com/tarista13/CS212/tree/main/Project_2', image:''},
+  { year: 'Fall 2021', project: 'Brick Breaker', description:'', madeAt: 'CS108', builtWith: 'Python', link:'https://github.com/tarista13/Brick-Breaker', image:''}
 ];
 
 @Component({
@@ -49,4 +53,19 @@ export class AllProjectsComponent {
       window.open(link, '_blank'); // Opens the link in a new tab
     }
   }
+
+  constructor(private router: Router) {}
+  navigateToProject(project: AllProjects): void {
+    this.router.navigate(['/project-description'], {
+      queryParams: {
+        name: project.project,
+        description: project.description || '',
+        year: project.year || '',
+        madeAt: project.madeAt || '',
+        builtWith: project.builtWith || '',
+        link: project.link || '',
+        image: project.image || '',
+      },
+    });
+  }  
 }
